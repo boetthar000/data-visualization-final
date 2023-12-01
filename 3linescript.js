a40a0b1a624c851c5d413a1fcf8a7768b64eb20e
Shawn Dai <79814208+JoinGame22@users.noreply.github.com>
2023-12-01 08:30:29 -0700

    Add files via upload

--- 3linescript.js
+++ 3linescript.js
@@ -9,133 +9,86 @@ document.addEventListener('DOMContentLoaded', function () {
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
-
+    
         d3.csv("./data/filtered_global_development.csv").then(function (rawData) {
-            const data = processDataForChart(rawData, country1, country2,country3, country4, country5, country6,options.attribute);
-
+            const data = processDataForChart(rawData, country1, country2, country3, country4, country5, country6, options.attribute);
+    
             const x = d3.scaleBand()
                 .range([0, width])
                 .padding(0.1);
-
+    
             const y = d3.scaleLinear()
                 .range([height, 0]);
-
-            const line1 = d3.line()
+    
+            const line = d3.line()
                 .x(d => x(d.year))
-                .y(d => y(d[country1]));
-
-            const line2 = d3.line()
-                .x(d => x(d.year))
-                .y(d => y(d[country2]));
-                
-                const line3 = d3.line()
-                .x(d => x(d.year))
-                .y(d => y(d[country3]));
-
-            const line4 = d3.line()
-                .x(d => x(d.year))
-                .y(d => y(d[country4]));
-
-                const line5 = d3.line()
-                .x(d => x(d.year))
-                .y(d => y(d[country5]));
-
-            const line6 = d3.line()
-                .x(d => x(d.year))
-                .y(d => y(d[country6]));
-
+                .y(d => y(d.value));
+    
             x.domain(data.map(d => d.year));
-            y.domain([0, d3.max(data, d => Math.max(d[country1], d[country2], d[country3],d[country4], d[country5], d[country6]))]);
-
+            y.domain([0, d3.max(data, d => d3.max([d[country1], d[country2], d[country3], d[country4], d[country5], d[country6]]))]);
+    
             // Line Charts
-            svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", line1)
-                .attr("stroke", options.color1)
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-
-            svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", line2)
-                .attr("stroke", options.color2)
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-
+            for (let i = 1; i <= 6; i++) {
+                const currentCountry = eval(`country${i}`);
+                const currentLine = d3.line()
+                    .x(d => x(d.year))
+                    .y(d => y(d[currentCountry]));
+    
                 svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", line3)
-                .attr("stroke", options.color3)
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-
-            svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", line4)
-                .attr("stroke", options.color4)
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-
-                svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", line5)
-                .attr("stroke", options.color5)
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-
-            svg.append("path")
-                .data([data])
-                .attr("class", "line")
-                .attr("d", line6)
-                .attr("stroke", options.color6)
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-
+                    .data([data])
+                    .attr("class", "line")
+                    .attr("d", currentLine)
+                    .attr("stroke", options[`color${i}`])
+                    .attr("stroke-width", 2)
+                    .attr("fill", "none");
+    
+                // Add points for current line
+                svg.selectAll(`.point${i}`)
+                    .data(data)
+                    .enter().append("circle")
+                    .attr("class", `point${i}`)
+                    .attr("cx", d => x(d.year))
+                    .attr("cy", d => y(d[currentCountry]))
+                    .attr("r", 5)
+                    .style("fill", options[`color${i}`]);
+            }
+    
             // Axes
             svg.append("g")
                 .attr("transform", `translate(0,${height})`)
                 .call(d3.axisBottom(x));
-
+    
             svg.append("g")
                 .call(d3.axisLeft(y).tickFormat(d3.format("~s")));
-
-            // Legend (similar to previous examples)
+    
+            // Legend
             const legend = svg.append("g")
                 .attr("class", "legend")
                 .attr("transform", `translate(${width - 100},${margin.top})`);
-
+    
             const countries = [
-                { name: country1, color: options.color1 },
-                { name: country2, color: options.color2 },
-                { name: country3, color: options.color3 },
-                { name: country4, color: options.color4 },
-                { name: country5, color: options.color5 },
-                { name: country6, color: options.color6 }
+                country1, country2, country3, country4, country5, country6
             ];
-
+    
             countries.forEach((country, index) => {
                 const legendRow = legend.append("g")
                     .attr("transform", `translate(0, ${index * 20})`);
-
+    
                 legendRow.append("rect")
                     .attr("width", 10)
                     .attr("height", 10)
-                    .attr("fill", country.color);
-
+                    .attr("fill", options[`color${index + 1}`]);
+    
                 legendRow.append("text")
                     .attr("x", 20)
                     .attr("y", 10)
                     .attr("text-anchor", "start")
                     .style("text-transform", "capitalize")
-                    .text(country.name);
+                    .text(country);
             });
         });
     }
+    
 
     createLineChart("#line1", "United States", "Philippines","China", "Kenya", "Canada", "Argentina" ,{ yText: "Mobile Cellular Subscriptions", color1: "blue", color2: "grey", color3: "red", color4: "purple",color5: "orange", color6: "green",attribute: 'Data.Infrastructure.Mobile Cellular Subscriptions' });
     createLineChart("#line2", "China", "Kenya",  "United States", "Philippines","Canada", "Argentina" ,{ yText: "Mobile Cellular Subscriptions", color1: "blue", color2: "grey", color3: "red", color4: "purple",color5: "orange", color6: "green", attribute: 'Data.Infrastructure.Mobile Cellular Subscriptions' });
